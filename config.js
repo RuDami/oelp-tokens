8e91f626e10b45450c5c426636dfb48725208de9
Andrei Debelev <Debelev@org.kraud.ru>
2022-07-26 11:35:23 +0300

    new config

--- config.js
+++ config.js
@@ -1,49 +1,42 @@
 module.exports = {
   format: {
-    // Adding a custom format to show how to get an alias's name.
     customFormat: function({dictionary, options}) {
-      return dictionary.allTokens.map(token => {
-        let value = JSON.stringify(token.value);
-        // new option added to decide whether or not to output references
-        if (options.outputReferences) {
-          // the `dictionary` object now has `usesReference()` and
-          // `getReferences()` methods. `usesReference()` will return true if
-          // the value has a reference in it. `getReferences()` will return
-          // an array of references to the whole tokens so that you can access its
-          // name or any other attributes.
-          if (dictionary.usesReference(token.original.value)) {
-            const reference = dictionary.getReferences(token.original.value);
-            value = reference.name;
+      return dictionary.allTokens.map(token => {        
+        if (token.name.includes('font-weight')) {
+          if (!token.name.indexOf('f')) {
+            return `$${token.name}: ${token.description};`
           }
+          return `$${token.name}: $font-weights-${token.value.toLowerCase().replace(' ', '-')};`
         }
-
-        return `export const ${token.name} = ${value};`
+        return `$${token.name}: ${token.value};`
       }).join(`\n`)
     }
   },
-  source: [`./tokens/light/**/*.json`],
+
+
+  source: ['tokens/**/*.json'],
   platforms: {
-    scss: {
-      transformGroup: `scss`,
-      buildPath: `./css/`,
+    js: {
+      buildPath: 'js/',
+      transformGroup: 'js',
       files: [{
-        destination: `variables.scss`,
-        format: `scss/variables`,
+        destination: 'variables.js',
+        format: 'javascript/es6',
         options: {
           outputReferences: true
         }
       }]
     },
-    js: {
-      transformGroup: `js`,
-      buildPath: `./js/`,
+    scss: {
+      transformGroup: 'scss',
+      buildPath: 'css/',
       files: [{
-        destination: `variables.js`,
-        format: `javascript/es6`,
+        destination: 'variables.scss',
+        format: 'customFormat',
         options: {
-          outputReferences: true
+          outputReferences: true,
         }
       }]
     }
   }
-}
+};
